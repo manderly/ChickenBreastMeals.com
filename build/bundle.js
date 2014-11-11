979c47f011a4ac10062436b60edc152c75ef8d2d
Mandi Rocket <mandi018@gmail.com>
2014-11-11 12:20:38 -0800

    Removing unused code, formatting cleanup

--- build/bundle.js
+++ build/bundle.js
@@ -227,87 +227,50 @@ module.exports = function(app) {
 module.exports = function(app) {
 	app.controller('cbmMainController', function($scope, $http, $location) {
 
-	$scope.siteName = "Chicken Breast Meals.com";
-	$scope.orderProp = 'cooktime';
+		$scope.siteName = "Chicken Breast Meals.com";
+		$scope.orderProp = 'cooktime';
 
-	$scope.getOptionsFor = function(propName) {
-		return ($scope.meals || []).map(function (meal) {
-			return meal[propName];
-		}).filter(function(meal, index, array) {
-			return array.indexOf(meal) === index;
-		});
-	};
+		$scope.getOptionsFor = function(propName) {
+			return ($scope.meals || []).map(function (meal) {
+				return meal[propName];
+			}).filter(function(meal, index, array) {
+				return array.indexOf(meal) === index;
+			});
+		};
 
-	//viewing and filtering meals by properties
-	//thanks: http://jsfiddle.net/ExpertSystem/wYfs4/
-	$scope.filterMeals = function(meal) {
-		//check if each (published) meal contains any mealOptions that exist in $scope.filter object
-		if (meal.published) {
-		    var matches = true;
-		    for (var name in $scope.filter) {
-		    	if ($scope.filter[name] && !meal.mealOptions[name]) {
-		    		matches = false;
-		    		break;
-		    	}
-		    }
-		    return matches;
-		}
-	};
+		//viewing and filtering meals by properties
+		//thanks: http://jsfiddle.net/ExpertSystem/wYfs4/
+		$scope.filterMeals = function(meal) {
+			//check if each (published) meal contains any mealOptions that exist in $scope.filter object
+			if (meal.published) {
+			    var matches = true;
+			    for (var name in $scope.filter) {
+			    	if ($scope.filter[name] && !meal.mealOptions[name]) {
+			    		matches = false;
+			    		break;
+			    	}
+			    }
+			    return matches;
+			}
+		};
 
-	$scope.viewRecipe = function(meal) {
-		console.log("Selecting this meal in meal-list-controller.js: " + meal);
-		$location.path('/recipe/' + meal.url);
-	};
+		$scope.viewRecipe = function(meal) {
+			console.log("Selecting this meal in meal-list-controller.js: " + meal);
+			$location.path('/recipe/' + meal.url);
+		};
 
-	//get, create, edit, delete
-	$scope.getMeals = function() {
-		$http.get('/db').success(function(data) {
-			$scope.meals = data;
-		})
-		.error(function(data) {
-			console.log("getMeals Error: " + data);
-		});
-	};
+		//get, create, edit, delete
+		$scope.getMeals = function() {
+			$http.get('/db').success(function(data) {
+				$scope.meals = data;
+			})
+			.error(function(data) {
+				console.log("getMeals Error: " + data);
+			});
+		};
 
-//removal candidates
-	$scope.createNewMeal = function(newMeal) {
-		$http.post('/db',newMeal)
-		.success(function(data) {
-			$scope.meals = data;
-			console.log("post success: " + data.title);
-		})
-		.error(function(data) {
-			console.log("error: " + data);
-		});
-	};
-
-//removal candidates
-	$scope.editMeal = function(existingMeal) {
-		$http.put('/db/' + existingMeal._id, existingMeal)
-		.success(function(data) {
-			$scope.meals = data;
-			console.log("Edited meal: " + data.title);
-		})
-		.error(function(data) {
-			console.log("error:" + data);
-		});
-	};
-
-//removal candidates
-	$scope.deleteExistingMeal = function(id) {
-		$http.delete('/db/'+ id)
-		.success(function(data) {
-			$scope.meals = data;
-			console.log("Successfully deleted a meal. " + data);
-		})
-		.error(function(data) {
-			console.log("error: " + data);
-		});
-	};
-
-	$scope.getMeals();
-});
-
+		$scope.getMeals();
+	});
 };
 },{}],4:[function(require,module,exports){
 'use strict';
