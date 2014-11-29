fd47e0ead7bcddd6001d91ba7f657ba7b48bb71b
Mandi Rocket <mandi018@gmail.com>
2014-11-29 13:43:39 -0800

    Testing login service with mocked login server and data

--- test/angular-testbundle.js
+++ test/angular-testbundle.js
@@ -31160,11 +31160,7 @@ describe('Service tests', function() {
 	beforeEach(angular.mock.module('cbmApp'));
 
 	describe('mealsServer tests', function() { 
-		it('has a working meals-server service', inject(function(mealsServer) {
-			expect(mealsServer).to.exist;
-		}));
-
-		it('creates a new meal', inject(function(mealsServer, $httpBackend) {
+		
 		var fakeMeal = {
 			name:'Fake Meal',
 			url:'fake-meal-url',
@@ -31187,23 +31183,63 @@ describe('Service tests', function() {
 			rating:5
 		};
 
-		$httpBackend
-			.expectPOST('/db', fakeMeal)
-			.respond(200);
+
+		it('has a working meals-server service', inject(function(mealsServer) {
+			expect(mealsServer).to.exist;
+		}));
+
+		it('creates a new meal with saveNewMeal(meal, image)', inject(function(mealsServer, $httpBackend) {
+			$httpBackend
+				.expectPOST('/db', fakeMeal)
+				.respond(200);
+
 			var succeeded;
 			mealsServer.saveNewMeal(fakeMeal,fakeMeal.image)
+				.then(function() {
+					succeeded = true;
+				});
+			$httpBackend.flush();
+			expect(succeeded).to.be.true;
+		}));
+
+		it('gets an existing meal using index()', inject(function(mealsServer, $httpBackend) {
+			$httpBackend
+				.expectGET('/db')
+				.respond(200);
+
+			var succeeded;
+			mealsServer.index()
 			.then(function() {
 				succeeded = true;
 			});
-		$httpBackend.flush();
-		expect(succeeded).to.be.true;
-		
+			$httpBackend.flush();
+			expect(succeeded).to.be.true;
 		}));
 	});
 
 	describe('userFactory tests', function() { 
 		it('has a user-factory service', inject(function(userFactory) {
 			expect(userFactory).to.exist;
+		}));
+
+		it('logs in using correct login credentials', inject(function(userFactory, $httpBackend) {
+
+			$httpBackend
+				.expectPOST('/login')
+				.respond({
+					token: 'testToken',
+					user: 'cbmadmin'
+				})
+
+			var succeeded;
+			userFactory.login('cbmadmin', 'p')
+			.then(function(response) {
+				if (response.data.user === 'cbmadmin' && response.data.token === 'testToken') {
+					succeeded = true;
+				}
+			});
+			$httpBackend.flush();
+			expect(succeeded).to.be.true;
 		}));
 	});
 
