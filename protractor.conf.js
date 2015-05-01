56f40571fa0aad3ba8e6c637d0b81da34c519b19
Mandi Grant <mandi018@gmail.com>
2015-04-30 19:35:35 -0700

    More travis tinkering, trying to find the right settings

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,10 +1,19 @@
 exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  capabilities: {
+      'browserName': 'chrome',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      'build': process.env.TRAVIS_BUILD_NUMBER,
+      'name': 'ngValidation Protractor Tests'
+    },
 
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 
 	specs: ['test/e2e/*.js'],
 
-	baseUrl: 'http://localhost:3000',
+	baseUrl: 'http://localhost:' + (process.env.HTTP_PORT || '3000'),
 
 	rootElement: '.cbm-app',
 
@@ -12,6 +21,7 @@ exports.config = {
 		onComplete: null,
 		isVerbose: true,
 		showColors: true,
-		includeStackTrace: true
+		includeStackTrace: true,
+    defaultTimeoutInterval: 30000
 	}
 };
